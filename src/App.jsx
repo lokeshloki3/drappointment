2afe0980edbbe9a474fd5dbe74543a7392c82251
lokeshloki3 <lokesh1992p@gmail.com>
2024-10-29 20:04:55 +0530

    route try

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,15 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/drappointment" element={<Navigate to="/" replace />} /> */}
-        <Route path="/drappointment" element={<Home />} />
+        <Route path="/drappointment" element={<Navigate to="/" replace />} />
+        {/* <Route path="/drappointment" element={<Home />} /> */}
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:categoryId" element={<CategoryPage />} />
         <Route path="/blogs/:categoryId/:contentId" element={<ContentPage />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/book" element={<BookAppointment />} />
-        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Footer />
     </div>
