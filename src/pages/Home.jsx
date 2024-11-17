f2cf2b17c067268172956f816c0fd40d426e4e0e
lokeshloki3 <lokesh1992p@gmail.com>
2024-11-17 18:46:20 +0530

    home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,44 @@ import Header from "../components/Header";
 import SpecialityMenu from "../components/SpecialityMenu";
 import reviews from "../assets/testimonialsData";
 import FeatureTable from "../components/FeatureTable";
+import { assets } from "../assets/assets";
+import ActionButton from "../components/ActionButton";
+import ImageSection from "../components/ImageSection";
 
-const Home = () => {
+const Home = ({ setIsModalOpen }) => {
   return (
     <div>
       <h1 className="text-center m-8 text-3xl mt-24 font-medium md:text-5xl">Welcome to Cortihear</h1>
-      <Header />
+      {/* Passing down setIsModalOpen */}
+      <Header setIsModalOpen={setIsModalOpen}/>
       <SpecialityMenu />
+
+      {/* Button to open the appointment modal */}
+      <ActionButton 
+        onClick={() => setIsModalOpen(true)} 
+        label="Click to book your online appointment" 
+      />
+      
       <FeatureTable />
+
+      {/* Button to open the appointment modal */}
+      <ActionButton 
+        onClick={() => setIsModalOpen(true)} 
+        label="Click to book your online appointment" 
+      />
+
+      {/* Image Section 1 */}
+      <ImageSection src={assets.book} alt="Book" />
+      
+      {/* Image Section 2 */}
+      <ImageSection src={assets.whycorti} alt="Why Cortihear" />
+
+      {/* Button to open the appointment modal */}
+      <ActionButton 
+        onClick={() => setIsModalOpen(true)} 
+        label="Click to book your online appointment" 
+      />
+
       <Testimonials reviews={reviews}/>
     </div>
   );
