9db7e17d9af781ad3b1b237caa0d354d611bcd6d
lokeshloki3 <lokesh1992p@gmail.com>
2024-11-17 18:40:55 +0530

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
@@ -9,13 +9,21 @@ import BookAppointment from './pages/BookAppointment';
 import CategoryPage from './pages/CategoryPage';
 import ContentPage from './pages/ContentPage';
 import Blogs from './pages/Blogs';
+import AppointmentModal from './components/AppointmentModal';
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 
 const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="mx-4 sm:mx-[10%]">
-      <Navbar />
+      {/* Pass `setIsModalOpen` to Navbar and Home components */}
+      <Navbar setIsModalOpen={setIsModalOpen} />
+      
+      {/* Modal for booking appointment */}
+      <AppointmentModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home setIsModalOpen={setIsModalOpen} />} />
         <Route path="/drappointment" element={<Navigate to="/" replace />} />
         {/* <Route path="/drappointment" element={<Home />} /> */}
         <Route path="/blogs" element={<Blogs />} />
@@ -24,6 +32,7 @@ const App = () => {
         <Route path="/about-us" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/book" element={<BookAppointment />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Footer />
