08819fa9bfc5b4dd71cfca5c7e1cc93ea1745913
lokeshloki3 <lokesh1992p@gmail.com>
2024-11-18 14:24:01 +0530

    blogs updated

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -5,12 +5,12 @@ import { categories } from '../assets/blogsData';
 const Blogs = () => {
   return (
     <div className='mb-10'>
-      <div className="text-center mt-16 text-3xl pt-10 text-gray-500 mb-8">
+      <div className="text-center mt-16 text-3xl pt-10 text-gray-500 mb-7">
         <p>
-          <span className="text-gray-700 font-medium">Blogs</span>
+          <span className="text-gray-700 font-medium">BLOGS</span>
         </p>
       </div>
-      <p className="text-gray-600 text-center">Browse through our blog categories</p>
+      <p className="text-gray-600 text-center text-lg sm:text-xl">Browse through our blog categories</p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mt-5">
         {Object.entries(categories).map(([categoryId, category]) => (
           <Link to={`/blogs/${categoryId}`} key={categoryId} className="block">
